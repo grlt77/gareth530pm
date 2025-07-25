fbd46ebcddcda9e94b0eea16882c7e895ca90e8d
grlt77 <194495867+grlt77@users.noreply.github.com>
2025-07-25 17:18:35 +0800

    Add files via upload

---
+++ ct06fri530-gareth-main/L01_redcircle.js
@@ -0,0 +1,21 @@
+function setup(){
+    createCanvas(1580, 960);
+    background(250,150,50);
+}
+
+function draw(){
+    fill(0, 0, 0);
+    ellipse(300,200,300,270);
+    fill(0, 0, 0);
+    ellipse(300,400,300,270);
+    fill(0, 0, 0);
+    ellipse(300,600,300,270);
+    fill(255, 0, 0);
+    ellipse(300,200,200,190);
+    fill(255, 255, 0);
+    ellipse(300,400,200,190);
+    fill(0, 255, 0);
+    ellipse(300,600,200,190);
+    rect(450,380,100,50)
+    // Todo: Challenge 3 Traffic Light
+}
---
+++ ct06fri530-gareth-main/L02_funshapes.js
@@ -0,0 +1,56 @@
+
+function setup(){
+  createCanvas(1500, 950)
+  background('skyblue')
+}
+
+
+function draw() {
+  // Challenge 1: An eye using ellipse() function
+  // fill('black');
+  // ellipse(400,300,600,300);
+  // fill('red');
+  // ellipse(400,300,150,300);
+  // fill('blue');
+  // ellipse(400,300,150,70)
+
+  // Challenge 2: A face using circle() function
+  // fill('brown');
+  // ellipse(150,166,100,100);
+  // fill('brown');
+  // ellipse(450,166,100,100);
+  // fill('brown');
+  // ellipse(300,300,300,300);
+  // fill('black');
+  // ellipse(220,250,50,50);
+  // fill('black');
+  // ellipse(380,250,50,50);
+  // fill('black');
+  // ellipse(300,350,160,80);
+
+
+  // Challenge 3: A house using rect() function
+  // fill('green');
+  // rect(200,200,400,300);
+  // fill('blue')
+  // ellipse(310,300,70,70)
+  // fill('blue')
+  // ellipse(490,300,70,70)
+  // fill('brown');
+  // rect(365,370,70,130);
+  // fill('black');
+  // ellipse(420,440,20,30);
+  // fill('red');
+  // triangle(80,200,720,200,400,10);
+  //quad(123,456,789,234,567,890,345,678)
+  fill('green')
+  quad(1000,800,500,800,300,950,1300,950)
+
+  // Challenge 4: A rocketship using triangle() function
+
+  // Challenge 5: A square face using quad() function
+
+  // Challenge 6: Add some colours to the drawings in the above challenges
+
+  // Challenge 7: Create a funny Pokemon
+}
