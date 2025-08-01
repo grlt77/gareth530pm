57918c090228fb7f92485051d522fc7791c5b5b9
grlt77 <194495867+grlt77@users.noreply.github.com>
2025-08-01 10:55:46 +0000

    Co-authored-by: DLEE <autom8ton@users.noreply.github.com>

--- ct06fri530-gareth-main/L04_mouseclick.js
+++ ct06fri530-gareth-main/L04_mouseclick.js
@@ -1,1 +1,48 @@
 // write your codes here
+let sc = 'red';
+let score = 0;
+function setup() {
+    createCanvas(1500, 700);
+    background(220);
+}
+
+function draw(){
+
+//     for (let c = 0;c<800;c=c+5){
+//     circle(c,c,100)
+//     fill (0,c,0)
+
+// }
+//     for (let b = 700;b<2;b=b-5){
+//     circle(b,0,100)
+//     fill (0,b,0)
+
+// }
+//fill (sc);
+//circle (width/2,height/2,500);
+
+textSize(30);
+textAlign(CENTER,CENTER);
+text("score: " + score,500,100);
+}
+
+function mousePressed(){
+    //sc = 'red';
+    //sc = color(random(255),random(255),random(255));
+    //score+=100;
+}
+//function mouseReleased(){
+//     //sc = 'blue';
+    //sc = color(random(255),random(255),random(255));
+//}
+
+function mouseMoved(){
+    //sc = color(random(255),random(255),random(255));
+    //fill(sc);
+    //circle(mouseX,mouseY,50);
+}
+function mouseDragged(){
+    sc = color(random(255),random(255),random(255));
+    fill(sc);
+    circle(mouseX+random(-10,10),mouseY+random(-10,10),random(30));
+}
