43bbf8bc90d729e87fee0e4587b814b78e4a4144
grlt77 <194495867+grlt77@users.noreply.github.com>
2025-08-15 10:48:49 +0000

    Co-authored-by: DLEE <autom8ton@users.noreply.github.com>

---
+++ ct06fri530-gareth-main/L06_practice.js
@@ -0,0 +1,29 @@
+let y = 200
+let x = 200
+let e = 50
+function setup() {
+    createCanvas(400, 400);
+
+}
+
+function draw(){
+    noStroke();
+    rect(x,y,10,10);
+fill(e);
+if (keyIsDown (87)){
+         y = y - 5;
+
+     }
+    if (keyIsDown (65)){
+         x = x - 5;
+     }
+     if (keyIsDown (83)){
+         y = y + 5;
+     }
+     if (keyIsDown (68)){
+         x = x + 5;
+     }
+    x = constrain(x,0,width - 10);
+    y = constrain(y,0,height - 10);
+
+}
--- ct06fri530-gareth-main/L06_spritesoundfx.js
+++ ct06fri530-gareth-main/L06_spritesoundfx.js
@@ -1,1 +1,58 @@
-// write your codes here
+let picoimg,bgmu,soundpop;
+let y=300;
+let x=300;
+function preload(){
+    picoimg = loadImage('assets/pico-a.png');
+    bgmu= loadSound('assets/.mp3');
+    soundpop=loadSound('assets/pop.mp3');
+
+
+}
+function setup() {
+    createCanvas(1500, 700);
+background('lightblue');
+bgmu.loop();
+}
+function draw(){
+background('lightblue');
+    image(picoimg,x,y,110,133);
+    if (keyIsDown (87)){
+         y = y - 5;
+
+     }
+    if (keyIsDown (65)){
+         x = x - 5;
+     }
+     if (keyIsDown (83)){
+         y = y + 5;
+     }
+     if (keyIsDown (68)){
+         x = x + 5;
+     }
+    x = constrain(x,0,width - 110);
+    y = constrain(y,0,height - 133);
+    if (x = 0){
+        background ('red')
+        soundpop.play();
+    }
+    if (x = width-110){
+        background ('red')
+        soundpop.play();
+    }
+    if (y = 0){
+        background ('red')
+        soundpop.play();
+    }
+    if (y = height-133){
+        background ('red')
+        soundpop.play();
+    }
+}
+
+function keyPressed(){
+soundpop.play();
+}
+
+function mousePressed(){
+    soundpop.play();
+}
